0e05cf02fbfae5f1d327e8cbd93c5c01ccfb899b
Night Chough <chicken.snuggles@gmail.com>
2021-05-04 17:58:01 -0500

    added GET routes for Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,11 +5,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'tag_type' }]
+    });
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tag_type' }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag with that id!' })
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
