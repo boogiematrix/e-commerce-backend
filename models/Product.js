f4221854a6b303e720a6f9e33aadff20b3a4663b
Night Chough <chicken.snuggles@gmail.com>
2021-05-05 12:28:42 -0500

    added POST PUT and DELETE routes to Tag

--- models/Product.js
+++ models/Product.js
@@ -24,7 +24,7 @@ Product.init(
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-
+        isDecimal: true,
       },
     },
     stock: {
@@ -32,7 +32,7 @@ Product.init(
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isDecimal: true,
+        isNumeric: true,
       },
     },
     category_id: {
